f018921b1c50d02a8c647ea9921be0a67a950753
Kaarthick Raja V <123342597+Morningstar135@users.noreply.github.com>
2024-04-25 09:20:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from "react-native";
 import Encrypt from "./Components/Encrypt";
 import Decrypt from "./Components/Decrypt";
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
- 
+ const Stack = createNativeStackNavigator();
   return (
-          <Encrypt />
-          //<Decrypt />
-     
+   <NavigationContainer >
+   <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} />
+   <Stack.Screen name="Encrypt" component={Encrypt}/>
+   <Stack.Screen name="Decrypt" component={Decrypt} />
+      </Stack.Navigator>
+   </NavigationContainer >
   );
 }
 
